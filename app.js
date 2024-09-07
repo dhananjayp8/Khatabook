d9329c9a65ecab0dc87ad575290af050c3c5b039
Dhananjay Puranik <dhananjaypuranik8@gmail.com>
2024-09-07 15:13:12 +0530

    created basic express setup

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+app.listen(3030, () => {
+  console.log("Server started");
+});
